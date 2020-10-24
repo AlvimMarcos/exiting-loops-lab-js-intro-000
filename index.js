a477433b23e31db5ea76d5f886591b31a3a83b9c
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-24 20:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 
-function breakOut(array, changeValue, stopValue) {
-  for (var i=0, l = array.length; i < l; i++){
-    if (array[i] === skipValue){
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0, l = array.length; i < l ; i++) {
+    if (array[i] === skipValue) {
       continue
     }
     array[i] = changeValue
   }
   return array
 }
+
+
+
 
 function findBy(array, findFn) {
   for (let i = 0, l = array.length; i < l; i++) {
