99444dfa90703e54dd029ba84069c39297ba1bdf
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-24 20:16:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function breakOut(array, changeValue, stopValue) {
   }
   return array
 }
+
+
+function breakOut(array, changeValue, stopValue) {
+  for (var i=0, l = array.length; i < l; i++){
+    if (array[i] === skipValue){
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
